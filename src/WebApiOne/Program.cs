fdb5ddbb1ee2a99a423e40d94de39214632c2399
Diego Martin <martind@vitessepsp.com>
2024-08-26 15:48:33 +0200

    Added scalar UI and cors to test it

--- src/WebApiOne/Program.cs
+++ src/WebApiOne/Program.cs
@@ -1,10 +1,29 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(
+    options =>
+        options
+            .AddPolicy(
+                "AllowAll",
+                policy =>
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()));
+
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.MapGet("/", () => "Hello World!");
-app.MapOpenApi();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.Run();
